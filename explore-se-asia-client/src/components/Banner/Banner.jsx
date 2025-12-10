ad414325bb84f43b91762b946da58173361a13cf
kawsar12759 <kawsar15-12759@diu.edu.bd>
2025-12-10 23:57:24 +0600

    updated intro section

--- explore-se-asia-client/src/components/Banner/Banner.jsx
+++ explore-se-asia-client/src/components/Banner/Banner.jsx
@@ -37,7 +37,7 @@ const Banner = () => {
                     </div>
                     <div className="bg-white rounded-2xl shadow-xl overflow-hidden transform transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:bg-[#f3f8f6] cursor-pointer">
                         <div className="relative">
-                            <img src="https://i.ibb.co.com/HDRybpXK/waterfall-1-11zon.webp" alt="Waterfall" className="w-full h-40 object-cover" />
+                            <img src="https://i.ibb.co.com/DDkkTcVW/waterfall-1-11zon-1-11zon.webp" alt="Waterfall" className="w-full h-40 object-cover" />
                             <div className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900/60 flex justify-center items-center text-center text-white">
                                 <h3 className="text-lg sm:text-xl font-semibold">Hidden Gems</h3>
                             </div>
--- explore-se-asia-client/src/components/Home/Home.jsx
+++ explore-se-asia-client/src/components/Home/Home.jsx
@@ -1,18 +1,61 @@
+import { Link } from "react-router-dom";
 import Banner from "../Banner/Banner";
 import Featured from "../Featured/Featured";
+import img1 from '../../assets/forest_119591.png'
+import img2 from '../../assets/emerald-temple_4193505.png'
+import img3 from '../../assets/man_11508976.png'
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <section className="bg-black py-16 px-8">
+            <section className="bg-gray-50 py-24 px-8">
                 <div className="container mx-auto text-center">
-                    <h2 className="text-2xl sm:text-3xl font-bold text-white mb-6">Welcome to ExploreSEAsia</h2>
-                    <p className="text-md sm:text-lg  text-white max-w-3xl mx-auto">
-                        Southeast Asia is a region of rich cultural diversity, stunning landscapes, and unforgettable experiences. From ancient temples and bustling markets to pristine beaches and lush jungles, ExploreSEAsia is your gateway to discovering the best that this enchanting part of the world has to offer.
+                    <h2 className="text-3xl sm:text-4xl font-bold text-teal-800 mb-8">
+                        Embark on Your Southeast Asia Adventure
+                    </h2>
+                    <p className="text-lg sm:text-xl text-gray-700 max-w-3xl mx-auto mb-8">
+                        Southeast Asia is more than a destination—it's an experience. From serene beaches to bustling cities, from ancient traditions to modern wonders, every corner of this region offers something unique. Let us guide you to the most captivating spots, where memories are made, and new adventures await.
                     </p>
+                    <div className="flex justify-center gap-8 mb-8">
+                        <div className="text-center">
+                            <div className="flex justify-center">
+                                <div className="bg-teal-800 w-36 h-36 text-white p-2 rounded-full mb-4">
+                                    <img className="h-full w-full" src={img1} alt="" />
+                                </div>
+                            </div>
+                            <h3 className="text-2xl font-semibold text-teal-800">Nature Escapes</h3>
+                            <p className="text-gray-600">Discover tranquil beaches, dense jungles, and towering mountains, perfect for nature lovers and adventurers.</p>
+                        </div>
+                        <div className="text-center">
+                            <div className="flex justify-center">
+                                <div className="bg-teal-800 w-36 h-36 text-white p-2 rounded-full mb-4">
+                                    <img className="h-full w-full" src={img2} alt="" />
+                                </div>
+                            </div>
+                            
+                            <h3 className="text-2xl font-semibold text-teal-800">Cultural Richness</h3>
+                            <p className="text-gray-600">Explore ancient temples, vibrant markets, and immerse yourself in the local traditions and festivals of Southeast Asia.</p>
+                        </div>
+                        <div className="text-center">
+                            <div className="flex justify-center">
+                                <div className="bg-teal-800 w-36 h-36 text-white p-2 rounded-full mb-4">
+                                    <img className="h-full w-full" src={img3} alt="" />
+                                </div>
+                            </div>
+                            <h3 className="text-2xl font-semibold text-teal-800">Adventurous Journeys</h3>
+                            <p className="text-gray-600">From thrilling hikes to water sports and more, Southeast Asia is a paradise for the adventurous traveler.</p>
+                        </div>
+                    </div>
+                    <Link to="/all-spots">
+                        <button className="bg-teal-800 hover:bg-teal-700 text-white py-3 px-8 rounded-full transition-all duration-300 shadow-lg">
+                            Start Exploring Now
+                        </button>
+                    </Link>
                 </div>
             </section>
+
+
             <Featured></Featured>
         </div>
     );
